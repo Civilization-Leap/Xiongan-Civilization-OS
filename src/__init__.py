06dace23d901336ec90d5cb82af31070e5a1426b
Civilization-Leap <axdwzx@me.com>
2026-01-17 22:51:15 +0800

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,18 @@
+```python
+# Copyright 2024 ZhongXin Wang
+# Licensed under the Apache License, Version 2.0
+
+"""
+Xiongan Civilization Operating System
+
+The digital infrastructure for the Xiongan Civilization Leap experiment.
+"""
+
+__version__ = "0.1.0"
+__author__ = "ZhongXin Wang"
+__email__ = ""
+__license__ = "Apache-2.0"
+
+# Core module exports
+__all__ = []
+```
