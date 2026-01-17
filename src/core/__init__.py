ba8d5bcac71286791c79aa52b7029678ae598772
Civilization-Leap <axdwzx@me.com>
2026-01-17 23:12:01 +0800

    Update __init__.py

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,2 +1,15 @@
+# Copyright 2024 ZhongXin Wang
+# Licensed under the Apache License, Version 2.0
 
- # Core module for Civilization OS
+"""
+Core modules for Xiongan Civilization OS.
+"""
+
+from .did import DID
+from .exceptions import (
+    CivilizationOSError,
+    DIDValidationError,
+    DIDNotFoundError
+)
+
+__all__ = ["DID", "CivilizationOSError", "DIDValidationError", "DIDNotFoundError"]
